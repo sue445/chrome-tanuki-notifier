55c59444efc94b683df405e233fc327bb85d5772
PENGBIN <pengbin@kingsoft.com>
2017-11-24 12:12:35 +0800

    add a test file

---
+++ test/trigger_test.js
@@ -0,0 +1,37 @@
+global.window = require("mithril/test-utils/browserMock.js")();
+global.document = window.document;
+
+const assert = require("power-assert");
+const trigger = require("../src/trigger.js");
+
+describe("trigger", () => {
+  it("works", () => {
+    const vnode = trigger.view({
+      state: {
+        projects: {
+          "219579": {
+            avatar_url: "https://gitlab.com/uploads/project/avatar/219579/image.jpg",
+            events: {
+              Commit: true,
+              Issue: true,
+              MergeRequest: true,
+              Milestone: true
+            },
+            name: "sue445/example"
+          },
+        },
+        new_milli_seconds: 1000,
+        
+        gitlab_path: "http://example.com",
+        gitlab: {
+          avatar_urls: {
+            1: "http://example.com/uploads/project/avatar/4/uploads/avatar.png",
+          }
+        }
+      }
+    });
+
+    assert(vnode.tag == "div");
+    assert(vnode.children.length > 0);
+  });
+});
